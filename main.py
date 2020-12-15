e0c600b7482a74363522b78603c7409beeece5fa
Rafael Schmidt <ralfe.schmidt@gmail.com>
2020-12-15 21:03:38 +0000

    Bottom bar with fuel pointer

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import pygame, sys, random
 
 pygame.init()
 
-screen_height = 480
+
+screen_height = 576 #480 w/o bar
 screen_width = 704
 
 screen = pygame.display.set_mode((screen_width,screen_height))
@@ -12,13 +13,14 @@ clock = pygame.time.Clock()
 bg_surface = pygame.image.load('images/bckg-1.jpeg').convert()
 bY = 0
 bY2 = bg_surface.get_height()
+fuel_bar_bg = pygame.image.load('images/bar test.png')
+fuel_bar_indicator = pygame.image.load('images/bar_indicators.png')
 
 #images of the player
 plane = pygame.image.load('images/plane-still.png')
 plane_left = pygame.image.load('images/plane-left.png')
 plane_right = pygame.image.load('images/plane-right.png')
 
-fuel_bar = pygame.image.load('images/fuel_bar.png')
 
 #images of the enemies
 choppa_l1, choppa_l2 = pygame.image.load('images/choppa_l1.png'), pygame.image.load('images/choppa_l2.png')
@@ -33,8 +35,8 @@ ship_right = pygame.image.load('images/ship_r.png')
 jet_left = pygame.image.load('images/jet_l.png')
 jet_right = pygame.image.load('images/jet_r.png')
 
-choices_left = ['jet_l.png','ship_l.png']
-choices_right = ['jet_r.png','ship_r.png']
+choices_left = ['jet_l.png','ship_l.png','choppa_l1.png']
+choices_right = ['jet_r.png','ship_r.png','choppa_r1.png']
 
 start = True
 
@@ -47,8 +49,13 @@ right = False
 
 #plane position in x axis , starting at the middle of screen
 plane_x = 350 - (int(width/2))
+plane_y = 420
 
-player_hitbox = pygame.Rect(plane_x, 420, plane.get_width(),plane.get_height())
+
+player_hitbox = pygame.Rect(plane_x, plane_y, plane.get_width(),plane.get_height())
+
+fuel_pointer = pygame.Rect(437, 505,12,49)
+
 
 #camera scrolling
 up_pressed = False
@@ -71,20 +78,24 @@ def generate_enemies(number,off,max_x=405,min_x=145):
         elif tmp[1] == -1:
             tmp.append(random.choice(choices_left))
         enemies_list.append(tmp)
-    print(enemies_list)
-    return enemies_list 
+    return enemies_list
 
 def draw_enemies(enemies_list):
-    
     for enemy in enemies_list:
-        enemy_rect = [enemy[0][0],enemy[0][1],pygame.image.load('images/'+enemy[2]).get_width(),pygame.image.load('images/'+enemy[2]).get_height()]
-        pygame.draw.rect(screen,(255,0,0),enemy_rect,2)
+        if not enemy[2][:6] == "choppa":
+            screen.blit(pygame.image.load('images/'+enemy[2]), enemy[0])
+        elif enemy[2] == "choppa_l1.png":
+            screen.blit(choppa_left[frame], enemy[0])
+        elif enemy[2] == "choppa_r1.png":
+            screen.blit(choppa_right[frame], enemy[0])
+            
+def check_collision(enemies_list):
+     for enemy in enemies_list:
+        enemy_rect = [enemy[0][0],enemy[0][1]-1,pygame.image.load('images/'+enemy[2]).get_width(),pygame.image.load('images/'+enemy[2]).get_height()+1]
+        pygame.draw.rect(screen,(0,255,0), enemy_rect,2)
+        pygame.display.update()
         if player_hitbox.colliderect(enemy_rect):
             return True
-        screen.blit(pygame.image.load('images/'+enemy[2]), enemy[0])
-
-
-
 
 def redrawWindow():
     if start:
@@ -94,17 +105,21 @@ def redrawWindow():
         screen.blit(bg_surface, (0,bY))
         screen.blit(bg_surface, (0,bY2))
     if right:
-        screen.blit(plane_right,(plane_x, 420))
+        screen.blit(plane_right,(plane_x, plane_y))
     elif left:
-        screen.blit(plane_left,(plane_x, 420))
+        screen.blit(plane_left,(plane_x, plane_y))
     else:
-        screen.blit(plane, (plane_x, 420))
+        screen.blit(plane, (plane_x, plane_y))
     draw_enemies(enemies_start)
     draw_enemies(enemies_start_off)
     move_enemies()
     pygame.draw.line(screen,(255,0,0),(0, bY),(704,bY))
     pygame.draw.line(screen,(255,0,0),(0, bY2),(704,bY2))
     pygame.draw.rect(screen,(255,0,0),player_hitbox,2)
+    screen.blit(fuel_bar_bg, (0,480))
+    #ponteiro de gasolina
+    pygame.draw.rect(screen, (252,252,84),fuel_pointer)
+    screen.blit(fuel_bar_indicator, (235,500))
     pygame.display.update()
 
 
@@ -141,41 +156,44 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
+    time = pygame.time.get_ticks()
+    frame = int((time/speed)%len(choppa_right))
+
     redrawWindow()
     checkScroll()
 
     bY += scroll_speed
     bY2 += scroll_speed
+
+
     
     #handle infinite scroll
+
     if bY > bg_surface.get_height():
         bY = -1*bg_surface.get_height()
         start = False
-        enemies_start = generate_enemies(5, True)
-        
+        enemies_start = generate_enemies(5, True) 
     if bY2 > bg_surface.get_height():
         bY2 = -1*bg_surface.get_height()
         enemies_start_off = generate_enemies(5, True)
 
-    if draw_enemies(enemies_start) or  draw_enemies(enemies_start_off):
-        break
+    # collision with enemies check
+    if check_collision(enemies_start) or  check_collision(enemies_start_off): 
+        pygame.quit()
+        quit()
 
-
-        
+    # keys handle
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT] and keys[pygame.K_RIGHT]:
         left = False
         right = False
-    elif keys[pygame.K_LEFT]: #test value
+    elif keys[pygame.K_LEFT]:
         plane_x-=vel
         player_hitbox.x -= vel
-
         left = True    
-    elif keys[pygame.K_RIGHT]: #test value
+    elif keys[pygame.K_RIGHT]:
         plane_x+=vel
-        player_hitbox.x +=vel
-
-        
+        player_hitbox.x +=vel 
         right = True
     else:
         left = False
