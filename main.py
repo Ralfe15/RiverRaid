82a0d7d92239726689c119f71d61ac2d7c794ec0
Rafael Schmidt <42819312+Ralfe15@users.noreply.github.com>
2020-12-21 13:51:09 -0300

    shoot action with bullet collision and fuel refil

--- main.py
+++ main.py
@@ -33,7 +33,12 @@ choppa_left = [choppa_l1, choppa_l2]
 choices_left = ['jet_l.png','ship_l.png','choppa_l1.png']
 choices_right = ['jet_r.png','ship_r.png','choppa_r1.png']
 
-fuel = pygame.image.load("images/fuel.png") #rememeber to scale 2x size at home
+fuel = pygame.image.load("images/fuel.png")
+
+bullet = pygame.image.load("images/bullet.png")
+bullets = []
+cooldown = 20
+cooldown_counter = 0
 
 start = True
 
@@ -66,10 +71,19 @@ def generate_enemies(number,off,max_x=405,min_x=145):
         tmp = []
         if not off:
             #generate enememies on main scene, with max height of 200 and minimum/maximum x specified on function call
-            tmp.append([random.randint(min_x,max_x), random.randint(0,200)])
+            gen_y = random.randint(0,200)
+            #check if an enemy has the
+            for j in enemies_list:
+                while gen_y in range(j[0][1], j[0][1]+20):
+                    gen_y = random.randint(0,200)
+            tmp.append([random.randint(min_x,max_x), gen_y])   
         elif off:
             #generate enememies on main scene, with max height of -400/minumum of 0 and minimum/maximum x specified on function call
-            tmp.append([random.randint(min_x,max_x), random.randint(-400,0)])
+            gen_y = random.randint(-400,0)
+            for j in enemies_list:
+                while gen_y in range(j[0][1], j[0][1]+20):
+                    gen_y = random.randint(-400,0)
+            tmp.append([random.randint(min_x,max_x), gen_y])
         tmp.append(random.choice([1,0, -1]))
         if tmp[1] == 1 or tmp[1] == 0:
             #load sprite acording to direction
@@ -79,6 +93,22 @@ def generate_enemies(number,off,max_x=405,min_x=145):
             tmp.append(random.choice(choices_left))
         enemies_list.append(tmp)
     return enemies_list
+
+def generate_fuel(number,off,max_x=405,min_x=145):
+    fuel_list = []
+    for i in range(number):
+        tmp = []
+        if not off:
+            tmp.append([random.randint(min_x,max_x), random.randint(0,200)])
+        elif off:
+            tmp.append([random.randint(min_x,max_x), random.randint(-400,0)])
+        fuel_list.append(tmp)
+    return fuel_list
+
+def draw_move_fuel(fuel_list):
+    for galoon in fuel_list:
+        screen.blit(pygame.image.load("images/fuel.png"), (galoon[0][0],galoon[0][1]))
+        galoon[0][1] += scroll_speed
 
 def draw_enemies(enemies_list):
     for enemy in enemies_list:
@@ -104,7 +134,6 @@ def check_fuel():
 
 def redrawWindow():
     global fuel_x
-
     if start:
         screen.blit(pygame.image.load('images/background.png').convert(), (0,bY))
         screen.blit(bg_surface, (0,bY2))
@@ -112,10 +141,16 @@ def redrawWindow():
         screen.blit(bg_surface, (0,bY))
         screen.blit(bg_surface, (0,bY2))
     if right:
+        draw_move_fuel(fuel_start)
+        draw_move_fuel(fuel_start_off)
         screen.blit(plane_right,(plane_x, plane_y))
     elif left:
+        draw_move_fuel(fuel_start)
+        draw_move_fuel(fuel_start_off)
         screen.blit(plane_left,(plane_x, plane_y))
     else:
+        draw_move_fuel(fuel_start)
+        draw_move_fuel(fuel_start_off)
         screen.blit(plane, (plane_x, plane_y))
     draw_enemies(enemies_start)
     draw_enemies(enemies_start_off)
@@ -129,6 +164,8 @@ def redrawWindow():
     pygame.draw.rect(screen, (252,252,84),pygame.Rect(fuel_x,504,12,49))
     #indicador de quantidade de gasolina
     screen.blit(fuel_bar_indicator, (235,500))
+    move_draw_bullets()
+    bullet_collision()
     pygame.display.update()
 
 
@@ -152,10 +189,72 @@ def checkScroll():
         elif scroll_speed < 1.5:
             scroll_speed += 0.08       
 
+def cooldown_handler():
+    global cooldown_counter
+    global cooldown
+    if cooldown_counter >= cooldown:
+        cooldown_counter = 0
+    else:
+        cooldown_counter += 1
+        
+def bullet_isoffscreen(curr):
+    return curr[1] <= screen_height and curr[1] >= 0
+
+def shoot():
+    global cooldown_counter
+    if cooldown_counter == 0:
+        bullets.append([plane_x+(width/2)-3, 420])
+        cooldown_counter = 1
+
+def move_draw_bullets():
+    cooldown_handler()
+    for i in bullets:
+        screen.blit(pygame.image.load("images/bullet.png"), (i[0], i[1]))
+        i[1] -= 15
+        if not bullet_isoffscreen(i):
+            bullets.remove(i)
+
+def bullet_collision():
+     for bullet in bullets:
+         for enemy in enemies_start:
+             enemy_rect = [enemy[0][0],enemy[0][1]-1,pygame.image.load('images/'+enemy[2]).get_width(),pygame.image.load('images/'+enemy[2]).get_height()+1]
+             bullet_rect = pygame.Rect(bullet[0],bullet[1], pygame.image.load("images/bullet.png").get_width(), pygame.image.load("images/bullet.png").get_height())
+             if bullet_rect.colliderect(enemy_rect):
+                bullets.remove(bullet)
+                enemies_start.remove(enemy)
+         for enemy in enemies_start_off:
+             enemy_rect = [enemy[0][0],enemy[0][1]-1,pygame.image.load('images/'+enemy[2]).get_width(),pygame.image.load('images/'+enemy[2]).get_height()+1]
+             bullet_rect = pygame.Rect(bullet[0],bullet[1], pygame.image.load("images/bullet.png").get_width(), pygame.image.load("images/bullet.png").get_height())
+             if bullet_rect.colliderect(enemy_rect):
+                bullets.remove(bullet)
+                enemies_start_off.remove(enemy)
+         for galoon in fuel_start:
+            galoon_rect = [galoon[0][0],galoon[0][1]-1,pygame.image.load("images/fuel.png").get_width(),pygame.image.load("images/fuel.png").get_height()+1]
+            bullet_rect = pygame.Rect(bullet[0],bullet[1], pygame.image.load("images/bullet.png").get_width(), pygame.image.load("images/bullet.png").get_height())
+            if bullet_rect.colliderect(galoon_rect):
+                bullets.remove(bullet)
+                fuel_start.remove(galoon)
+         for galoon in fuel_start_off:
+            galoon_rect = [galoon[0][0],galoon[0][1]-1,pygame.image.load("images/fuel.png").get_width(),pygame.image.load("images/fuel.png").get_height()+1]
+            bullet_rect = pygame.Rect(bullet[0],bullet[1], pygame.image.load("images/bullet.png").get_width(), pygame.image.load("images/bullet.png").get_height())
+            if bullet_rect.colliderect(galoon_rect):
+                bullets.remove(bullet)
+                fuel_start_off.remove(galoon)
+                
+def collision_with_fuel(fuel_list):
+    global fuel_x
+    for galoon in fuel_list:
+            galoon_rect = [galoon[0][0],galoon[0][1]-1,pygame.image.load("images/fuel.png").get_width(),pygame.image.load("images/fuel.png").get_height()+1]
+            if player_hitbox.colliderect(galoon_rect) and fuel_x < 437:
+                fuel_x += 0.6
+         
+    
 
 
 enemies_start = generate_enemies(3, False)
 enemies_start_off = generate_enemies(5, True)
+fuel_start = generate_fuel(1,False)
+fuel_start_off = generate_fuel(3, True)
 
 
 speed = 60
@@ -178,18 +277,17 @@ while True:
     #move fuel pointer
     fuel_x -= 0.125
     
-
-
-    
     #handle infinite scroll
 
     if bY > bg_surface.get_height():
         bY = -1*bg_surface.get_height()
         start = False
-        enemies_start = generate_enemies(5, True) 
+        enemies_start = generate_enemies(5, True)
+        fuel_start = generate_fuel(3,True)
     if bY2 > bg_surface.get_height():
         bY2 = -1*bg_surface.get_height()
         enemies_start_off = generate_enemies(5, True)
+        fuel_start_off = generate_fuel(3, True)
 
     # collision with enemies check
     if check_collision(enemies_start) or  check_collision(enemies_start_off): 
@@ -199,7 +297,8 @@ while True:
     if check_fuel():
         pygame.quit()
         quit()
-
+    collision_with_fuel(fuel_start)
+    collision_with_fuel(fuel_start_off)
     # keys handle
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT] and keys[pygame.K_RIGHT]:
@@ -207,7 +306,6 @@ while True:
         right = False
     elif keys[pygame.K_LEFT]:
         plane_x-=vel
-        
         player_hitbox.x -= vel
         left = True    
     elif keys[pygame.K_RIGHT]:
@@ -224,6 +322,8 @@ while True:
     else:
         up_pressed = False
         down_pressed = False
+    if keys[pygame.K_SPACE]:
+        shoot()
 
 
     clock.tick(speed)
